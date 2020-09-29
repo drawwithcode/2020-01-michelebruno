31bf7ccc31eb8eb76d2d6b15b5552ca61e8cc948
michelebruno <bm.michelebruno@gmail.com>
2020-09-29 19:51:07 +0200

    First changes

--- sketch.js
+++ sketch.js
@@ -1,12 +1,24 @@
-function preload(){
-  // put preload code here
-}
-
 function setup() {
-  createCanvas(windowWidth,windowHeight)
-  // put setup code here
+    createCanvas(windowWidth, windowHeight)
+
+    angleMode(DEGREES);
+
+    colorMode(RGB);
+
+    background(0,0,0);
 }
 
 function draw() {
-  // put drawing code here
+    // put drawing code here
+}
+
+function polygon(x, y, radius, npoints) {
+    let angle = 360 / npoints;
+    beginShape();
+    for (let a = 0; a < 360; a += angle) {
+        let sx = x + cos(a) * radius;
+        let sy = y + sin(a) * radius;
+        vertex(sx, sy);
+    }
+    endShape(CLOSE);
 }
