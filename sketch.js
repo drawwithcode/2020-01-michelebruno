2aae85b6b6ffbad2dc56277e995be9ca06d71d3d
michelebruno <bm.michelebruno@gmail.com>
2020-09-29 22:31:46 +0200

    Almost ready

--- sketch.js
+++ sketch.js
@@ -1,15 +1,85 @@
+let sideSlider,
+    repeatSlider,
+    wUnit,
+    hUnit;
+
 function setup() {
-    createCanvas(windowWidth, windowHeight)
+    createCanvas(windowWidth, windowHeight);
 
     angleMode(DEGREES);
 
     colorMode(RGB);
 
-    background(0,0,0);
+    wUnit = width / 6;
+    hUnit = height / 6;
+
+    background(0, 0, 0);
+
+    frameRate(30);
+
+    let repeat = createDiv('<h2>Repeat it</h2>');
+    repeat.style('color', 'white')
+    repeat.position(width - wUnit, height / 2);
+    repeat.size(wUnit / 3);
+
+
+    repeatSlider = createSlider(1, 10, 6, 1);
+    repeatSlider.size(wUnit / 3);
+    // repeatSlider.position(width - wUnit, height / 2)
+    repeatSlider.parent(repeat);
+
+    let side = createDiv('<h2>Shape it</h2>');
+    side.style('color', 'white');
+    side.position(wUnit / 2, height / 2);
+    side.size(wUnit / 3);
+
+    sideSlider = createSlider(2, 13, 6, 1);
+    sideSlider.parent(side);
+    sideSlider.style('width', '100%');
+
+
+    noFill();
+    strokeWeight(3);
+    stroke('white');
 }
 
 function draw() {
-    // put drawing code here
+
+    background(0, 0, 0, 18);
+    translate(width / 2, height / 2);
+
+    let h = sin(frameCount * 2) * wUnit,
+        d = repeatSlider.value();
+
+
+    for (let i = 0; i < d; i++) {
+        push();
+
+        rotate(i * 360 / d);
+
+        let g = map(mouseY, 0, windowHeight, 80, 200);
+        let r = map(mouseX, 0, windowWidth, 80, 255);
+        let b = map(r * g + 1, 0, 255 * 255, 0, 250);
+
+        stroke(lerpColor(color(b, g, r), color(r, g, b), i / d));
+
+        translate(h, 0);
+
+        let p = sideSlider.value()
+        f = p > 12 ? ellipse : polygon;
+
+        f(sin(frameCount) * 150, -sin(frameCount) * 150, (cos(frameCount) + 1.1) * wUnit/3, p > 12 ? undefined : p)
+
+        pop();
+    }
+
+
+}
+
+function drawpoly() {
+    polygon(sin(frameCount) * 150, -sin(frameCount) * 150, (cos(frameCount) + 1.1) * 80, sideSlider.value())
+
+    // ellipse(cos(frameCount) * 150, sin(frameCount) * 50, (cos(frameCount) + 1.5) * 100);
 }
 
 function polygon(x, y, radius, npoints) {
